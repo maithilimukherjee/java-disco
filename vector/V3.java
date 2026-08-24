b581fab0ccf3cae4b2955f3dc71f5117955f2718
Maithili Mukherjee <maithilimukherjee@outlook.com>
2026-08-24 20:02:04 +0530

    Add V3 class for counting occurrences of an integer in a Vector

---
+++ vector/V3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class V3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Vector<Integer> v = new Vector<>();
+
+        for (int i = 0; i < n; i++) {
+            v.add(sc.nextInt());
+        }
+
+        int x = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < v.size(); i++) {
+            if (v.get(i).equals(x)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
